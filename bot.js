a2d5b6d2f8044bd6494ebec767be70b8bc1e4447
spam1312 <46295956+spam1312@users.noreply.github.com>
2019-09-28 14:00:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,6 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const client3 = new Discord.Client();
-const client4 = new Discord.Client();
-const client5 = new Discord.Client();
-const client6 = new Discord.Client();
-const client7 = new Discord.Client();
-const client8 = new Discord.Client();
-const client9 = new Discord.Client();
-const client10 = new Discord.Client();
-const client11 = new Discord.Client();
-const client12 = new Discord.Client();
-const client13 = new Discord.Client();
 console.log("Scrpit By Dream");
 
 
@@ -29,95 +18,6 @@ channel.send(`w5445454545`);
 }, 100000000)
 })
 
-client3.on("ready", () => {
-let channel =     client.channels.get("627452206626504705")
-setInterval(function() {
-channel.send(`w5445454545`);
-}, 100000000)
-})
-
-client4.on("ready", () => {
-let channel =     client.channels.get("627452206626504705")
-setInterval(function() {
-channel.send(`w5445454545`);
-}, 100000000)
-})
-
-client5.on("ready", () => {
-let channel =     client.channels.get("627452206626504705")
-setInterval(function() {
-channel.send(`w5445454545`);
-}, 100000000)
-})
-
-client6.on("ready", () => {
-let channel =     client.channels.get("627452206626504705")
-setInterval(function() {
-channel.send(`w5445454545`);
-}, 100000000)
-})
-
-client7.on("ready", () => {
-let channel =     client.channels.get("627452206626504705")
-setInterval(function() {
-channel.send(`w5445454545`);
-}, 100000000)
-})
-
-client8.on("ready", () => {
-let channel =     client.channels.get("627452206626504705")
-setInterval(function() {
-channel.send(`w5445454545`);
-}, 100000000)
-})
-
-client9.on("ready", () => {
-let channel =     client.channels.get("627452206626504705")
-setInterval(function() {
-channel.send(`w5445454545`);
-}, 100000000)
-})
-
-client10.on("ready", () => {
-let channel =     client.channels.get("627452206626504705")
-setInterval(function() {
-channel.send(`w5445454545`);
-}, 100000000)
-})
-
-client11.on("ready", () => {
-let channel =     client.channels.get("627452206626504705")
-setInterval(function() {
-channel.send(`w5445454545`);
-}, 100000000)
-})
-
-client12.on("ready", () => {
-let channel =     client.channels.get("627452206626504705")
-setInterval(function() {
-channel.send(`w5445454545`);
-}, 100000000)
-})
-
-client13.on("ready", () => {
-let channel =     client.channels.get("627452206626504705")
-setInterval(function() {
-channel.send(`w5445454545`);
-}, 100000000)
-})
-
 
 client.login(process.env.BOT_TOKEN);
 client.login(process.env.BOT_TOKEN2);
-client.login(process.env.BOT_TOKEN3);
-client.login(process.env.BOT_TOKEN4);
-client.login(process.env.BOT_TOKEN5);
-client.login(process.env.BOT_TOKEN6);
-client.login(process.env.BOT_TOKEN7);
-client.login(process.env.BOT_TOKEN8);
-client.login(process.env.BOT_TOKEN9);
-client.login(process.env.BOT_TOKEN10);
-client.login(process.env.BOT_TOKEN11);
-client.login(process.env.BOT_TOKEN12);
-client.login(process.env.BOT_TOKEN13);
-
